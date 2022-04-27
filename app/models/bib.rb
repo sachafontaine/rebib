5d44919e7f1d11e7edc87ee435a1ef21eb0135e2
Sacha Fontaine <sachafontaine92500@gmail.com>
2022-04-27 16:13:20 -0400

    cleaning repo

--- app/models/bib.rb
+++ app/models/bib.rb
@@ -2,10 +2,8 @@ class Bib < ApplicationRecord
   belongs_to :user
   belongs_to :race
   monetize :price_cents
-
   validates :price, presence: true
   validates :available, presence: true
-
   enum available: {
     free: 1,
     unfree: 2
